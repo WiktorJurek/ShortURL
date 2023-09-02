aa1dc9b64b2287e278118dd42325afabb09707c4
WiktorJurek <38005557+WiktorJurek@users.noreply.github.com>
2023-09-02 15:40:53 +0200

    Project structure and basic logic

---
+++ api/routes/shorten.go
@@ -0,0 +1,30 @@
+package routes
+
+import(
+	"time"
+)
+
+type request struct {
+	URL				string				`json:"url"`
+	CustomShort		string				`json:"short"`
+	Expiry			time.Duration		`json:"expiry"`
+}
+
+type response struct {
+	URL					string			`json:"url"`
+	CustomShort			string			`json:"short"`
+	Expiry				time.Duration	`json:"expiry"`
+	XRateRemaining		int				`json:"rate_limit"`
+	XRateLimitRest		time.Duration	`json:"rate_limit_rest"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+
+	body := new(request)
+
+	if err := c.BodyParser(&body); err!= nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"cannot parse JSON"})
+	}
+
+	
+}
